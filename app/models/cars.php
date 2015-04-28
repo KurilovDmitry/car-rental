c75b9b0a5538f7e988ba0b91a9d9a6c9625dfdc3
Red <shizgarick@gmail.com>
2015-04-28 08:55:44 +0300

    Some SQL scripts added

--- app/models/cars.php
+++ app/models/cars.php
@@ -7,6 +7,61 @@ class cars extends \core\model {
     }
 
     public function getCars() {
-        return $this->_db->select('SELECT Id, Model, Cost, RentPrice FROM car');
+        return $this->_db->select('SELECT C.* FROM CAR C JOIN CAR_MODEL CM IN C.ID = CM.CAR_ID JOIN MODEL M IN CM.MODEL_ID = M.ID');
     }
+
+    public function getAllCars() {
+        return $this->_db->select('SELECT C.ID, QM.MODEL, GROUP_CONCAT(QP.DESCRIPTION SEPARATOR) PROPERTIES FROM CAR C
+                                    JOIN (
+                                      SELECT M.MODEL, CM.CAR_ID CAR_ID FROM MODEL M
+                                      JOIN CAR_MODEL CM ON M.ID = CM.MODEL_ID
+                                    ) AS QM
+                                    JOIN (
+                                      SELECT P.DESCRIPTION DESCRIPTION, CP.CAR_ID CAR_ID FROM PROPERTY P
+                                      JOIN CAR_PROPERTY CP ON P.ID = CP.PROPERTY_ID
+                                    ) AS QP
+                                    WHERE C.ID = QM.CAR_ID
+                                    AND C.ID = QP.CAR_ID
+                                    GROUP BY C.ID');
+    }
+
+    public function getRentedCars() {
+        return $this->_db->select('SELECT C.ID, QM.MODEL, GROUP_CONCAT(QP.DESCRIPTION SEPARATOR) PROPERTIES FROM CAR C
+                                    JOIN (
+                                      SELECT M.MODEL, CM.CAR_ID CAR_ID FROM MODEL M
+                                      JOIN CAR_MODEL CM ON M.ID = CM.MODEL_ID
+                                    ) AS QM
+                                    JOIN (
+                                      SELECT P.DESCRIPTION DESCRIPTION, CP.CAR_ID CAR_ID FROM PROPERTY P
+                                      JOIN CAR_PROPERTY CP ON P.ID = CP.PROPERTY_ID
+                                    ) AS QP
+                                    WHERE C.ID = QM.CAR_ID
+                                    AND C.ID = QP.CAR_ID
+                                    AND C.ID IN (
+                                      SELECT D.CAR_ID FROM DEAL D
+                                      WHERE D.FINISH_DATE > CURRENT_TIMESTAMP
+                                    )
+                                    GROUP BY C.ID');
+    }
+
+    public function getNotRentedCars() {
+        return $this->_db->select('SELECT C.ID, QM.MODEL, GROUP_CONCAT(QP.DESCRIPTION SEPARATOR) PROPERTIES FROM CAR C
+                                    JOIN (
+                                      SELECT M.MODEL, CM.CAR_ID CAR_ID FROM MODEL M
+                                      JOIN CAR_MODEL CM ON M.ID = CM.MODEL_ID
+                                    ) AS QM
+                                    JOIN (
+                                      SELECT P.DESCRIPTION DESCRIPTION, CP.CAR_ID CAR_ID FROM PROPERTY P
+                                      JOIN CAR_PROPERTY CP ON P.ID = CP.PROPERTY_ID
+                                    ) AS QP
+                                    WHERE C.ID = QM.CAR_ID
+                                    AND C.ID = QP.CAR_ID
+                                    AND C.ID NOT IN (
+                                      SELECT D.CAR_ID FROM DEAL D
+                                      WHERE D.FINISH_DATE > CURRENT_TIMESTAMP
+                                    )
+                                    GROUP BY C.ID');
+    }
+
+    
 }
